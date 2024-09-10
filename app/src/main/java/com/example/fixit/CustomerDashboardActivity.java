9ad058d2a35e175a8d7d94b42ccd13a4a41adc9a
devraj-05 <devrajpsagathiya@gmail.com>
2024-09-10 11:13:55 +0530

    commit from devraj

--- app/src/main/java/com/example/fixit/CustomerDashboardActivity.java
+++ app/src/main/java/com/example/fixit/CustomerDashboardActivity.java
@@ -14,5 +14,7 @@ public class CustomerDashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_dashboard);
+
+        //code from devraj laptop
     }
 }
