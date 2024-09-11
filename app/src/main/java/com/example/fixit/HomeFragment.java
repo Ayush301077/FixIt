20448ae631f034d094951e25359ab8a54dabfffd
Ayush301077 <luckyvsn0904@gmail.com>
2024-09-11 18:07:53 +0530

    service provider: created all demo fragments which change on the basis of icon clicked

---
+++ app/src/main/java/com/example/fixit/HomeFragment.java
@@ -0,0 +1,34 @@
+package com.example.fixit;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class HomeFragment extends Fragment {
+
+    TextView textview;
+    public HomeFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
--- app/src/main/java/com/example/fixit/ServiceProviderDashboard.java
+++ app/src/main/java/com/example/fixit/ServiceProviderDashboard.java
@@ -1,21 +1,89 @@
 package com.example.fixit;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class ServiceProviderDashboard extends AppCompatActivity {
 
+    BottomNavigationView bnView;
+    FrameLayout frameLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_provider_dashboard);
 
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Home");
+        ColorDrawable colorDrawable
+                = new ColorDrawable(Color.parseColor("#F44336"));
+        actionBar.setBackgroundDrawable(colorDrawable);
 
+        bnView = findViewById(R.id.bnView);
+        frameLayout = findViewById(R.id.frameLayout);
 
+        bnView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int id = item.getItemId();
+
+                if(id == R.id.home)
+                {
+                    loadFragment(new HomeFragment(), 1);
+
+                }
+                else if(id == R.id.aboutus)
+                {
+                    loadFragment(new AboutUsFragment(), 0);
+
+                }
+                else if(id == R.id.chats)
+                {
+                    loadFragment(new ChatsFragment(), 0);
+
+                }
+                else if(id == R.id.earnings)
+                {
+                    loadFragment(new EarningsFragment(), 0);
+
+                }
+                else
+                {
+                    loadFragment(new ProfileFragment(), 0);
+                }
+
+                return true;
+            }
+        });
+        bnView.setSelectedItemId(R.id.home);
+
+    }
+
+    public void loadFragment(Fragment fragment, int flag)
+    {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        if(flag ==1)
+            ft.add(R.id.frameLayout, fragment);
+        else
+            ft.replace(R.id.frameLayout, fragment);
+        ft.commit();
     }
 }
